8ec1d247cf0a7e1df45387f0689892ae1813af7f
samyazajnr28-sketch <samyazajnr28@gmail.com>
2026-03-08 03:12:07 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,16 +4,16 @@ const settings = {
   // Array fallback: splits string by comma, or uses default array
   prefixes: process.env.PREFIXES ? process.env.PREFIXES.split(',') : ['.', '!', '/', '#'],
   
-  packname: process.env.PACKNAME || 'MEGA-MD',
+  packname: process.env.PACKNAME || 'SAMYAZA-MD',
   author: process.env.AUTHOR || 'GlobalTechInfo',
-  timeZone: process.env.TIMEZONE || 'Asia/Karachi',
-  botName: process.env.BOT_NAME || "MEGA-MD",
-  botOwner: process.env.BOT_OWNER || 'Qasim Ali',
-  ownerNumber: process.env.OWNER_NUMBER || '923051391007',
+  timeZone: process.env.TIMEZONE || 'Africa/Nairobi',
+  botName: process.env.BOT_NAME || "SAMYAZA-MD",
+  botOwner: process.env.BOT_OWNER || 'Samyaza Jnr',
+  ownerNumber: process.env.OWNER_NUMBER || '254706610185',
   giphyApiKey: process.env.GIPHY_API_KEY || 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: process.env.COMMAND_MODE || "public",
   
-  maxStoreMessages: Number(process.env.MAX_STORE_MESSAGES) || 20,
+  maxStoreMessages: Number(process.env.MAX_STORE_MESSAGES) || 200,
   tempCleanupInterval: Number(process.env.CLEANUP_INTERVAL) || 1 * 60 * 60 * 1000,
   storeWriteInterval: Number(process.env.STORE_WRITE_INTERVAL) || 10000,
   
